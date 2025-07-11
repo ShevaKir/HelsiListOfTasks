bb1b5906da87e662ec7a355fc4f43dab1d1870be
Kyrylo Shevchenko <kir971@gmail.com>
2025-07-11 15:09:55 +0300

    Added swagger

--- HelsiListOfTasks.WebApi/Program.cs
+++ HelsiListOfTasks.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace HelsiListOfTasks.WebApi;
 
 public static class Program
@@ -6,7 +8,25 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Task Lists API",
+                Version = "v1"
+            });
+        });
+
         var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
         app.UseHttpsRedirection();
 
