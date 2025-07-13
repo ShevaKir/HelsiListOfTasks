cb07bf6f9e5609f4dcdd881ae7f04d637b06208f
Kyrylo Shevchenko <kir971@gmail.com>
2025-07-13 11:39:46 +0300

    Implemented sharing repository

--- HelsiListOfTasks.Domain/Repositories/ITaskListSharingRepository.cs
+++ HelsiListOfTasks.Domain/Repositories/ITaskListSharingRepository.cs
@@ -2,7 +2,7 @@ namespace HelsiListOfTasks.Domain.Repositories;
 
 public interface ITaskListSharingRepository
 {
-    Task AddShareAsync(int listId, int targetUserId);
-    Task RemoveShareAsync(int listId, int targetUserId);
-    Task<List<int>> GetSharedUserIdsAsync(int listId);
+    Task AddShareAsync(string taskListId, string targetUserId);
+    Task RemoveShareAsync(string taskListId, string targetUserId);
+    Task<List<string>> GetSharedUserIdsAsync(string taskListId);
 }
---
+++ HelsiListOfTasks.Infrastructure/Mongo/MongoTaskListSharingRepository.cs
@@ -0,0 +1,34 @@
+using HelsiListOfTasks.Domain.Models;
+using HelsiListOfTasks.Domain.Repositories;
+using MongoDB.Driver;
+
+namespace HelsiListOfTasks.Infrastructure.Mongo;
+
+public class MongoTaskListSharingRepository(MongoDbContext context) : ITaskListSharingRepository
+{
+    private readonly IMongoCollection<TaskList> _collection = context.TaskLists;
+
+    public async Task AddShareAsync(string taskListId, string targetUserId)
+    {
+        var filter = Builders<TaskList>.Filter.Eq(x => x.Id, taskListId);
+        var update = Builders<TaskList>.Update.AddToSet<>(x => x.SharedWithUserIds, targetUserId);
+        await _collection.UpdateOneAsync(filter, update);
+    }
+
+    public async Task RemoveShareAsync(string taskListId, string targetUserId)
+    {
+        var filter = Builders<TaskList>.Filter.Eq(x => x.Id, taskListId);
+        var update = Builders<TaskList>.Update.Pull<>(x => x.SharedWithUserIds, targetUserId);
+        await _collection.UpdateOneAsync(filter, update);
+    }
+
+    public async Task<List<string>> GetSharedUserIdsAsync(string taskListId)
+    {
+        var taskList = await _collection
+            .Find(x => x.Id == taskListId)
+            .Project(x => x.SharedWithUserIds)
+            .FirstOrDefaultAsync();
+
+        return taskList ?? [];
+    }
+}
